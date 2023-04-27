4a55bf908ea359ff6f74b82c90537a6d6e350028
Alexey Paramonov <paramonov_tama@mail.ru>
2023-04-27 12:54:27 +0300

    init commit

---
+++ src/main/java/by/paramonov/firstrailway/FirstrailwayApplication.java
@@ -0,0 +1,13 @@
+package by.paramonov.firstrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstrailwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstrailwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/paramonov/firstrailway/FirstrailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package by.paramonov.firstrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstrailwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
